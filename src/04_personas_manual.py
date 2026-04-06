2430298ea18d76157b255e5de53aeddcf2a18ec1
Mohammad Mustafidur Rahman <naimyu@my.yorku.ca>
2026-04-06 19:36:55 -0400

    Update 04_personas_manual.py

---
+++ src/04_personas_manual.py
@@ -0,0 +1,7 @@
+"""
+Manual personas creation step.
+
+This file represents the manual process of creating personas
+based on review groups. Personas are written directly into:
+personas/personas_manual.json
+"""
